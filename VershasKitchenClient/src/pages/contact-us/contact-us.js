88b20c4ed550709538ddd3c19b13a3f63b941ae3
Mayank Goyal <mgoel0007@gmail.com>
2021-07-09 23:38:17 +0530

    imroved contactus UI

--- VershasKitchenClient/src/pages/contact-us/contact-us.js
+++ VershasKitchenClient/src/pages/contact-us/contact-us.js
@@ -5,7 +5,6 @@ import chefContact from '../../assets/img/chef-contact1.png'
 
 const api = "api/"
 const path = api + 'sendDetails';
-
 class ContactUs extends Component {
 
     constructor(props) {
@@ -19,33 +18,38 @@ class ContactUs extends Component {
     }
 
     shouldComponentUpdate(nextProps, nextState) {
-        return true;
+        if (this.state.name != nextState.name || this.state.phoneNumber != nextState.phoneNumber ||
+            this.state.email != nextState.email || this.state.message != nextState.message)
+            return false;
+        else
+            return true;
     }
 
     submitForm(e) {
+        var form = document.getElementById('contactUsForm')
         let formIsValid = true;
         const data = {
             "name": this.state.name ? this.state.name : '',
-            "phoneNumber": this.state.phonenumber ? this.state.phonenumber : '',
+            "phoneNumber": this.state.phoneNumber ? this.state.phoneNumber : '',
             "email": this.state.email ? this.state.email : '',
             "message": this.state.message ? this.state.message : ''
         }
-        if (data.name == '' || data.phonenumber == '') {
+        if (data.name == '' || data.phoneNumber == '') {
             formIsValid = false;
         }
-        var self = this;
         if (formIsValid) {
             e.preventDefault();
+            var self = this;
             axios({
                 method: "post",
                 url: path,
                 data: data
-                // headers: { "Content-Type": "multipart/form-data" },
             })
                 .then(function (response) {
                     //handle success
                     console.log(response);
                     self.setState({ thankyou: true });
+                    form.reset();
                 })
                 .catch(function (response) {
                     //handle error
@@ -67,39 +71,43 @@ class ContactUs extends Component {
                         <img src={chefContact} alt='Contact us' />
                     </div>
                     <div className='contact-us-submision'>
-                        <h3>Contact Us</h3>
-                        <form id='contactUsForm'>
-                            <div className='form-row'>
-                                <div className='form-group col-md-6'>
-                                    <label for="name">Your Name</label>
-                                    <input type="text" className='contact-us-input' required id="name" name="name" placeholder="Your name.." onChange={this.handleChange} />
+                        { !this.state.thankyou &&
+                            <Fragment>
+                            <h3>Contact Us</h3>
+                            <form id='contactUsForm' ref={this.formRef}>
+                                <div className='form-row'>
+                                    <div className='form-group col-md-6'>
+                                        <label for="name">Your Name</label>
+                                        <input type="text" className='contact-us-input' required id="name" name="name" placeholder="Your name.." onChange={this.handleChange} />
+                                    </div>
+                                    <div className='form-group col-md-6'>
+                                        <label for="phoneNumber">Phone Number </label>
+                                        <input type="text" id="phoneNumber" className='contact-us-input' required name="phoneNumber" placeholder="Your phone no.." onChange={this.handleChange} />
+                                    </div>
                                 </div>
-                                <div className='form-group col-md-6'>
-                                    <label for="phonenumber">Phone Number </label>
-                                    <input type="text" id="phonenumber" className='contact-us-input' required name="phonenumber" placeholder="Your phone no.." onChange={this.handleChange} />
+                                <div className='form-row'>
+                                    <div className='form-group col-md-12'>
+                                        <label for="lname">Email</label>
+                                        <input type="text" id="lname" className='contact-us-input' name="email" placeholder="Your email address.." onChange={this.handleChange} />
+                                    </div>
                                 </div>
-                            </div>
-                            <div className='form-row'>
-                                <div className='form-group col-md-12'>
-                                    <label for="lname">Email</label>
-                                    <input type="text" id="lname" className='contact-us-input' name="email" placeholder="Your email address.." onChange={this.handleChange} />
+                                <div className='form-row'>
+                                    <div className='form-group col-md-12'>
+                                        <label for="subject">Message</label>
+                                        <textarea id="subject" className='contact-us-input' name="message" placeholder="Write something.." onChange={this.handleChange}></textarea>
+                                    </div>
                                 </div>
-                            </div>
-                            <div className='form-row'>
-                                <div className='form-group col-md-12'>
-                                    <label for="subject">Message</label>
-                                    <textarea id="subject" className='contact-us-input' name="message" placeholder="Write something.." onChange={this.handleChange}></textarea>
+                                <div className='form-row'>
+                                    <div className='form-group col-md-12'>
+                                        <input type="submit" className='contact-us-submit' onClick={this.submitForm} />
+                                    </div>
                                 </div>
-                            </div>
-                            <div className='form-row'>
-                                <div className='form-group col-md-12'>
-                                    <input type="submit" className='contact-us-submit' onClick={this.submitForm} />
-                                </div>
-                            </div>
-                        </form>
-                        <br/>
-                        {this.state.thankyou && <div><h2>Thank You For Contacting Us. We will reach out to you soon.</h2></div>}
+                            </form>
+                            </Fragment>
+                       }
+                        {this.state.thankyou && <div className='thankyou-msg'><div>Thank you for contacting us. We will reach out to you soon.</div></div>}
                     </div>
+
                 </div>
             </Fragment>
         );
